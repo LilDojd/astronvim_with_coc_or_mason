1bf44752d25792d062aa85435544e8b24ff3f1d4
LilDojd <yawner@protonmail.com>
2024-02-21 13:14:33 +0400

    My own config

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     features = {
       -- Configuration table of features provided by AstroLSP
-      autoformat = false, -- enable or disable auto formatting on start
+      autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
       lsp_handlers = true, -- enable/disable setting of lsp_handlers
       semantic_tokens = true, -- enable/disable semantic token highlighting
@@ -14,7 +14,14 @@ return {
     -- Configuration options for controlling formatting with language servers
     formatting = {
       -- control auto formatting on save
-      format_on_save = false,
+      format_on_save = {
+        -- enable or disable format on save globally
+        enabled = true,
+        -- enable format on save for specified filetypes only
+        allow_filetypes = {},
+        -- disable format on save for specified filetypes
+        ignore_filetypes = {},
+      },
       -- disable formatting capabilities for specific language servers
       disabled = {},
       -- default format timeout
