48d4d23c6ef690ec89b9717afc930df981b4f9d6
LilDojd <yawner@protonmail.com>
2024-02-21 18:17:12 +0400

    autoformat3

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,5 +18,5 @@ vim.opt.scrolloff = 5 -- keep 3 lines when scrolling
 vim.g.mapleader = " "
 vim.g.maplocalleader = ""
 vim.g.resession_enabled = true
-vim.g.autoformat = true
-vim.g.autoformat_enabled = true
+vim.g.autoformat = false
+vim.g.autoformat_enabled = false
